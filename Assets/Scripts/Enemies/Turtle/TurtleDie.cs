1fb5fc6dd862220d965c368c1d6cb7fa0fd6a828
LongNguyen <49540130+long06031999@users.noreply.github.com>
2022-01-09 20:08:24 +0700

    turtle enemies
    turtle enemies

--- Assets/Scripts/Enemies/Turtle/TurtleDie.cs
+++ Assets/Scripts/Enemies/Turtle/TurtleDie.cs
@@ -4,40 +4,87 @@ using UnityEngine;
 
 public class TurtleDie : MonoBehaviour
 {
-    private Vector2 positionDie;
-    public GameObject turtleDie;
-    private GameObject mario;
+    private float velocity = 15f;
+    private bool isMoveLeft = true;
+    private Vector2 direction;
+    private bool isFastMove = false;
 
-    private void Awake()
+    private void Start()
     {
-        mario = GameObject.FindGameObjectWithTag("Player");
+        direction = transform.position;
+        InvokeRepeating("time", 0.2f, 0.2f);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void time()
     {
-        positionDie = transform.localPosition;
+        if (transform.position.x == direction.x)
+        {
+            onDirection();
+        }
+        else
+        {
+            direction = transform.position;
+        }
     }
 
+    void onDirection()
+    {
+        isMoveLeft = !isMoveLeft;
+        Vector2 direction = transform.localScale;
+        direction.x *= -1;
+        transform.localScale = direction;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 position = transform.localPosition;
+        if (isMoveLeft)
+        {
+            position.x -= velocity * Time.deltaTime;
+        }
+        else
+        {
+            position.x += velocity * Time.deltaTime;
+        }
+        transform.position = position;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Player" && collision.contacts[0].normal.y < 0)
+        if (collision.collider.tag == "Player" && !isFastMove)
         {
-            Destroy(gameObject);
-            Instantiate(turtleDie, transform.position, Quaternion.identity);
-            turtleDie.transform.localPosition = positionDie;
-        }
-        else if (collision.collider.tag == "Player" && (collision.contacts[0].normal.x < 0 || collision.contacts[0].normal.x > 0))
-        {
-            if (mario.GetComponent<MarioController>().level > 0)
+            if(collision.contacts[0].normal.x > 0)
             {
-                mario.GetComponent<MarioController>().level -= 1;
-                mario.GetComponent<MarioController>().isChangeMario = true;
-                Destroy(gameObject);
+                //direction = Vector2.right;
+                isMoveLeft = false;
+                isFastMove = true;
             }
             else
             {
-                Destroy(mario);
+                //direction = Vector2.left;
+                isMoveLeft = true;
+                isFastMove = true;
+            }
+            if(isFastMove && collision.contacts[0].normal.y < 0) {
+                Destroy(gameObject);
+            }
+        }
+        if(isFastMove && collision.collider.tag == "Player")
+        {
+            MarioController marioController = collision.gameObject.GetComponent<MarioController>();
+            if (marioController)
+            {
+                if (marioController.level == 0)
+                {
+                    Destroy(collision.gameObject);
+                }
+                else
+                {
+                    marioController.level -= 1;
+                    marioController.isChangeMario = true;
+                }
+                Destroy(gameObject);
             }
         }
     }
--- Assets/Scripts/Enemies/Turtle/TurtleEnemie.cs
+++ Assets/Scripts/Enemies/Turtle/TurtleEnemie.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class TurtleEnemie : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 positionDie;
+    public GameObject turtleDie;
+    private GameObject mario;
+
+    private void Awake()
     {
-        
+        mario = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        positionDie = transform.localPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player" && collision.contacts[0].normal.y < 0)
+        {
+            Destroy(gameObject);
+            Instantiate(turtleDie, transform.position, Quaternion.identity);
+            turtleDie.transform.localPosition = positionDie;
+        }
+        else if (collision.collider.tag == "Player" && (collision.contacts[0].normal.x < 0 || collision.contacts[0].normal.x > 0))
+        {
+            if (mario.GetComponent<MarioController>().level > 0)
+            {
+                mario.GetComponent<MarioController>().level -= 1;
+                mario.GetComponent<MarioController>().isChangeMario = true;
+                Destroy(gameObject);
+            }
+            else
+            {
+                Destroy(mario);
+                Destroy(gameObject);
+            }
+        }
     }
 }
