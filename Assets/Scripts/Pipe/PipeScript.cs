1a444a45a9f6233a9bd7b3a824b501e17aab4975
GithubVanLinh <vovanlinhleo1999@gmail.com>
2022-01-18 17:52:38 +0700

    nothing

--- Assets/Scripts/Pipe/PipeScript.cs
+++ Assets/Scripts/Pipe/PipeScript.cs
@@ -5,47 +5,47 @@ using UnityEngine;
 public class PipeScript : MonoBehaviour
 {
 
-    public bool isOnPipe = false;
-    MarioController marioController;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public bool isOnPipe = false;
+  MarioController marioController;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.contacts[0].normal.y < 0 && collision.gameObject.tag == "Player")
-        {
-            marioController = collision.gameObject.GetComponent<MarioController>();
-            marioController.isOnPipe = true;
-            marioController.pipe = gameObject;
-        }
-    }
+  // Update is called once per frame
+  void Update()
+  {
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (marioController)
-        {
-            marioController.isOnPipe = false;
-            marioController.pipe = null;
-            marioController = null;
-        }
-    }
+  }
 
-    public bool OnPipe()
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.contacts[0].normal.y < 0 && collision.gameObject.tag == "Player")
     {
-        return isOnPipe;
+      marioController = collision.gameObject.GetComponent<MarioController>();
+      marioController.isOnPipe = true;
+      marioController.pipe = gameObject;
     }
+  }
 
-    public void Action()
+  private void OnCollisionExit2D(Collision2D collision)
+  {
+    if (marioController)
     {
-        marioController.transform.position = (Vector2)transform.GetChild(0).transform.position + new Vector2(0, 5);
+      marioController.isOnPipe = false;
+      marioController.pipe = null;
+      marioController = null;
     }
+  }
+
+  public bool OnPipe()
+  {
+    return isOnPipe;
+  }
+
+  public void Action()
+  {
+    marioController.transform.position = (Vector2)transform.GetChild(0).transform.position + new Vector2(0, 5);
+  }
 }
